5488f3be80b8c66ab5584cfae7d9297a71ed5065
IFoxH <oldsuw@gmail.com>
2017-06-19 17:29:29 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,13 +3,11 @@ var result = window.confirm('準備OK？');
 //結果を表示
 document.getElementById('choice').textContent = result;
 
-
-
-while(true){
-  //答えを用意
+//答えを用意
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
-  
+
+while(true){
 var message;
 if(answer === number){
   message = 'あたり！';
@@ -21,6 +19,7 @@ if(answer === number){
 }else{
   message = '0～3の数字を入力してください';
 }
+  var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
 }
 
 //結果を表示
