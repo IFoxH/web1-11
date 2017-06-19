b8f6634563a015970c807fb360cb6fb3e517bff7
IFoxH <oldsuw@gmail.com>
2017-06-19 17:19:48 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -7,11 +7,10 @@ document.getElementById('choice').textContent = result;
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
 
-while(0){
+while(answer==number){
 var message;
 if(answer === number){
   message = 'あたり！';
-  return 0;
 }else if (answer < number){
   message = '残念！もっと大きいです';
 }else if (number < answer){
