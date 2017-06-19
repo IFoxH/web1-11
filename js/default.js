8fee637227702aacd369cda8e1cef2460c6605cc
IFoxH <oldsuw@gmail.com>
2017-06-19 17:10:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,8 @@
+//確認ダイアログを表示
+var result = window.confirm('準備OK？');
+//結果を表示
+document.getElementById('choice').textContent = result;
+
 //答えを用意
 var number = Math.floor(Math.random()*3);
 var answer = parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
